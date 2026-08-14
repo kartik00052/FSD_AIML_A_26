de3c093c3562d712451d8e5f89510c6976303e89
Kartik00052 <kartiksharma18852@gmail.com>
2026-08-14 14:38:23 +0530

    React is added

--- JS_DOM/index.js
+++ JS_DOM/index.js
@@ -1,8 +1,16 @@
-const child=dcoument.createElement('div');
-child.setAttribute("classname","card");
-const image =document.createElement("img");
-const h2=document.createElement("h2");
-h2.innerText="Price: 500/-"
-child.appendChild(image);
-child.appendChild(h2);
+function Book(){
+    const child =document.createElement("div");
+    child.setAttribute("class","card")
+    const image=document.createElement("img");
+    image.setAttribute("src","https://th.bing.com/th/id/OIP.l5EiUfa6V9Ivp1dwjNoNJAHaLH?w=122&h=183&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3")
+    image.setAttribute("width","380px   ");
+    image.setAttribute("height","380px   ");
+    
+
+    const h2=document.createElement("h2");
+    h2.innerText="Price: 500/-";
+    child.appendChild(image);
+    child.appendChild(h2);
+}
 const parent=document.getElementById("root");
+parent.appendChild(Book());
---
+++ react cdn app/index.js
@@ -0,0 +1,11 @@
+const Data=[
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"},
+    {image:"", Price:"3259/-"}
+]
+function Book(props){
+    const image=React.createElement("image",{src:props.image,with:"50px",height:"50px"});
+    const h2=React.createElement("h2",{color:"red"},"Price: "+props.price);
+    const child=React.createElement("div",{classname:"card"},[image,h2]);
+}
