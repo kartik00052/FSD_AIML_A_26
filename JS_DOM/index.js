3906221d7551af159599ba5209b596714d7b5e8a
Kartik00052 <kartiksharma18852@gmail.com>
2026-08-14 13:39:37 +0530

    book store dom

---
+++ JS_DOM/index.js
@@ -0,0 +1,8 @@
+const child=dcoument.createElement('div');
+child.setAttribute("classname","card");
+const image =document.createElement("img");
+const h2=document.createElement("h2");
+h2.innerText="Price: 500/-"
+child.appendChild(image);
+child.appendChild(h2);
+const parent=document.getElementById("root");
