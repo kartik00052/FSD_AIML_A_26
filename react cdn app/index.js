a2b1454e62a06d7aab827f0fcdfbfe215761e110
Kartik00052 <kartiksharma18852@gmail.com>
2026-08-20 14:06:52 +0530

    image replaceed to img

--- react cdn app/index.js
+++ react cdn app/index.js
@@ -5,7 +5,8 @@ const Data=[
     {image:"", Price:"3259/-"}
 ]
 function Book(props){
-    const image=React.createElement("image",{src:props.image,with:"50px",height:"50px"});
+    const image=React.createElement("img",{src:props.image,with:"50px",height:"50px"});
     const h2=React.createElement("h2",{color:"red"},"Price: "+props.price);
     const child=React.createElement("div",{classname:"card"},[image,h2]);
+    return child;
 }
